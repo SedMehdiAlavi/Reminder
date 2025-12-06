4874add4686906dcfa46d68aa63a6877271e836e
mehdi <alavisedmehdi@gmail.com>
2025-12-06 12:10:36 +0330

    feat: implement empty methods in ReminderManager

--- manager.py
+++ manager.py
@@ -4,3 +4,18 @@ class ReminderManager:
 
     def add_reminder(self, reminder):
         pass
+
+    def remove_reminder(self, reminder):
+        pass
+
+    def list_reminders(self):
+        pass
+
+    def reminder_group(self, group):
+        pass
+
+    def execute_all(self):
+        pass
+
+    def find_reminder(self, reminder):
+        pass
